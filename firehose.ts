3358f249c9f17897cd414a49b205b8836a505f03
marukun712 <marukun530@gmail.com>
2025-10-09 22:10:06 +0900

    update

--- firehose.ts
+++ firehose.ts
@@ -2,3 +2,16 @@ import { Firehose } from "@aikyo/firehose";
 
 const firehose = new Firehose(8080);
 await firehose.start();
+
+//各トピックをサブスクライブ
+await firehose.subscribe("messages", (data) => {
+  firehose.broadcastToClients(data);
+});
+
+await firehose.subscribe("queries", (data) => {
+  firehose.broadcastToClients(data);
+});
+
+await firehose.subscribe("actions", (data) => {
+  firehose.broadcastToClients(data);
+});
